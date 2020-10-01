b5a49fd2232843a5bd40c0668496af7802321a8e
steadfasterX <>
2020-09-30 14:22:01 -1000

    CPH1859: prop: Fix Graphic Glitches

--- system_prop.mk
+++ system_prop.mk
@@ -29,6 +29,7 @@ debug.performance.tuning=1
 debug.egl.profiler=1 
 debug.egl.hw=1
 debug.composition.type=gpu 
+debug.hwui.renderer=opengl
 
 # Zygote preforking
 persist.device_config.runtime_native.usap_pool_enabled=true
